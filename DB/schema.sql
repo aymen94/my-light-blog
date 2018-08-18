bd9e50507825590f548e1c88f50b4032dbb359f8
Aymen Naghmouchi <aymenadvance@gmail.com>
2018-08-18 21:16:21 +0200

    sql schema for blog
    Signed-off-by: Aymen Naghmouchi <aymenadvance@gmail.com>

---
+++ DB/schema.sql
@@ -0,0 +1,31 @@
+-- Database: My light blog --
+
+CREATE SCHEMA my_light_blog;
+
+-- user table --
+CREATE TABLE my_light_blog.`user`(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(20) NOT NULL,
+    lastname  VARCHAR(25),
+    email VARCHAR(40) UNIQUE NOT NULL,
+    `password` CHAR(128)
+);
+
+-- category table --
+CREATE TABLE my_light_blog.category(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) UNIQUE NOT NULL
+);
+
+-- post table --
+CREATE TABLE my_light_blog.post(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	`user` INT NOT NULL,
+	category INT NOT NULL,
+	title VARCHAR(200),
+	body TEXT,
+    `date` TIMESTAMP,
+	FOREIGN KEY (`user`) REFERENCES `user`(id),
+	FOREIGN KEY (`category`) REFERENCES `category`(id)
+);
+
